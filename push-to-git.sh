c6ab8f363b466514526b95e40909aab1ecb77366
Seilmast <magnus_stordal@hotmail.com>
2023-09-14 09:02:02 +0200

    git_init

--- push-to-git.sh
+++ push-to-git.sh
@@ -3,4 +3,4 @@ git add --all
 echo "Init description" 
 read git_init
 git commit -m git_init
-git push github.com/BenBjorsvik/MMIV-Data-Contest.git magnus-branch:magnus-branch
+git push magnus-branch magnus-branch:magnus-branch
